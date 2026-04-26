887967bdc3d805bd857505c68a42389d984b8fb0
Gezang <118105169+Gezang@users.noreply.github.com>
2026-04-26 17:27:17 +1000

    Implemented BiLSTM

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Model_Linear import linear_model
 from ModelLSTM import main_lstm
+from ModelBiLSTM import main_bilstm
 
 if __name__ == "__main__":
-    linear_model()
-    main_lstm()
+    # linear_model()
+    # main_lstm()
+    main_bilstm()
